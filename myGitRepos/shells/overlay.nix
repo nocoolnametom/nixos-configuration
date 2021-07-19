1d3c79567398c617ad82a35ffff838c7e92982d1
Tom Doggett <tom@nocoolnametom.com>
2021-07-19 11:37:44 -0700

    Adding PHP 7.1 shell to overlay

--- myGitRepos/shells/overlay.nix
+++ myGitRepos/shells/overlay.nix
@@ -2,7 +2,7 @@
   myGitShells = let shells = super.callPackage ./default.nix { };
   in rec {
     inherit (shells)
-      node php72 php72-redis php72AndNode php73 php73-redis php73AndNode php74
+      node php71 php72 php72-redis php72AndNode php73 php73-redis php73AndNode php74
       zg-apops zg-crm-messagetemplate-bundle zg-crm-search-index-batch-job
       zg-dispatcher-model zg-goodcop zg-leadrouting-handler zg-web-crm;
   };
